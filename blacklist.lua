fe10ffaa791c0def6e8362fda243b14a77e671a2
= <undertakers_help@yahoo.com>
2017-12-26 21:48:21 +0100

    Blacklist changed.

--- blacklist.lua
+++ blacklist.lua
@@ -7,23 +7,8 @@
 
 login_blacklist = {
 	"Admin",
-	"admin",
-	"ADMIN",
-	"aDMIN",
 	"Administrator",
-	"administrator",
-	"ADMINISTRATOR",
-	"aDMINISTRATOR",
-	"TEST",
-	"tEST",
 	"Test",
-	"test", 
 	"Guest",
-	"GUEST",
-	"gUEST",
-	"guest",
 	"Gast", 
-	"GAST",
-	"gAST",
-	"gast"
 }
